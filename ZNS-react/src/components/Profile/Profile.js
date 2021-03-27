56b38d0069de87f32cabe58af1b4ff05c2c90c23
Brett <colbr243@gmail.com>
2021-03-27 19:20:03 +1300

    Got the profile looking closer to the design

--- ZNS-react/src/components/Profile/Profile.js
+++ ZNS-react/src/components/Profile/Profile.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import CopyInput from './CopyInput'
+import CopyInput from '../CopyInput/CopyInput.js'
 
-import ProfileStyle from '../styles/Profile.module.css'
+import ProfileStyle from './Profile.module.css'
 
 import qr from './assets/qr.png'
 import eth from './assets/eth.svg'
@@ -17,20 +17,12 @@ const Profile = (props) => {
 			</div>
 			<div className={ProfileStyle.body}>
 				<div>
-					<img src={dp} />
-					<span>Joe Dimaggio</span>
-					<span>0://wilderworld.joed</span>
+					<img className={ProfileStyle.dp} src={dp} />
+					<span className={ProfileStyle.name}>Frank Wilder</span>
+					<a className={ProfileStyle.endpoint}>0://wilderworld.frank</a>
 				</div>
 				<div>
-					<p>Hey I’m Joe and I like staring into the night sky and imagining myself in another galaxy. I’m so passionate about space travel that I spend the majority of my time making animated short films about it. With the magic of CGI, I can make worlds and journeys so real that I can almost taste the syntheic beef that comes out of the assembler!<br /><br />Join me on one or all of my journeys, I welcome you aboard!</p>
-				</div>
-			</div>
-			<div className={ProfileStyle.payment}>
-				<img src={qr} />
-				<div>
-					<img src={eth} />
-					<span>Your Ethereum Address</span>
-					<CopyInput value='bc1wiioasoidijjafs8wjlasdi98sdff099sd' />
+					<p>Hey I’m Frank and I like staring into the night sky and imagining myself in another galaxy. I’m so passionate about space travel that I spend the majority of my time making animated short films about it. With the magic of CGI, I can make worlds and journeys so real that I can almost taste the synthetic beef that comes out of the assembler!<br/><br/>Join me on one or all of my journeys, I welcome you aboard!</p>
 				</div>
 			</div>
 		</div>
