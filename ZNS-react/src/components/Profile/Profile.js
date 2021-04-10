3581ecbfe27028f3d1c6ec04bf49a108b0433214
Brett <colbr243@gmail.com>
2021-04-10 16:53:18 +1200

    :sparkles: Added shop container

--- ZNS-react/src/components/Profile/Profile.js
+++ ZNS-react/src/components/Profile/Profile.js
@@ -4,6 +4,8 @@ import CopyInput from '../CopyInput/CopyInput.js'
 
 import ProfileStyle from './Profile.module.css'
 
+import { Shop } from 'containers'
+
 import qr from './assets/qr.png'
 import eth from './assets/eth.svg'
 import dp from './assets/wilder.jpg'
@@ -12,9 +14,7 @@ const Profile = (props) => {
 
 	return (
 		<div className={`${ProfileStyle.profile} blur border-primary border-rounded`}>
-			<div className={ProfileStyle.header}>
-				<h3>Profile</h3>
-			</div>
+			<h1 className={`glow-text-white`}>Profile</h1>
 			<div className={ProfileStyle.body}>
 				<div>
 					<img className={ProfileStyle.dp} src={dp} />
@@ -25,6 +25,7 @@ const Profile = (props) => {
 					<p>Hey I’m Frank and I like staring into the night sky and imagining myself in another galaxy. I’m so passionate about space travel that I spend the majority of my time making animated short films about it. With the magic of CGI, I can make worlds and journeys so real that I can almost taste the synthetic beef that comes out of the assembler!<br/><br/>Join me on one or all of my journeys, I welcome you aboard!</p>
 				</div>
 			</div>
+			<Shop />
 		</div>
 	)
 }
--- ZNS-react/src/containers/Shop/Shop.js
+++ ZNS-react/src/containers/Shop/Shop.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import styles from './Shop.module.css'
+
+import { TextButton, NFTCard } from 'components'
+
+import NFTService from 'services/nfts.js'
+
 
 const Shop = (props) => {
 
+    const loggedInAs = 'Frank Wilder'
+    const [ NFTs, setNFTs ] = useState(NFTService.getOwnedBy(loggedInAs))
+
+    const getOwnedBy = () => {
+        setNFTs(NFTService.getOwnedBy(loggedInAs))
+    }
+
+    const getCreatedBy = () => {
+        setNFTs(NFTService.getCreatedBy(loggedInAs))
+    }
+
+    return(
+        <div className={styles.Shop}>
+            <div className={styles.Sections}>
+                <TextButton 
+                    toggleable={true}
+                    click={getOwnedBy}
+                >NFTs You Own</TextButton>
+                <TextButton 
+                    toggleable={true}
+                    click={getCreatedBy}
+                >NFTs You've Made</TextButton>
+                {/* <TextButton toggleable={true}>Offers</TextButton> */}
+            </div>
+            <ul>
+                { NFTs.map(n => 
+                    <li key={n.name}><NFTCard NFT={n} /></li>
+                ) }
+            </ul>
+        </div>
+    )
 }
 
 export default Shop
