01b65c5c408e525b1ca3591043b73cf2d56aa13f
Brett <colbr243@gmail.com>
2021-04-10 18:12:01 +1200

    :lipstick: Got Profile/shop looking more like the Figma file

--- ZNS-react/src/components/Cards/NFTCard/NFTCard.js
+++ ZNS-react/src/components/Cards/NFTCard/NFTCard.js
@@ -5,12 +5,14 @@ import styles from './NFTCard.module.css'
 const NFTCard = (props) => {
     return(
         <div className={`${styles.NFTCard} border-rounded`}>
-            <div className={styles.Creator}>
-                <div
-                    style={{backgroundImage: `url(${props.NFT.creator.img})`}} 
-                ></div>
-                <span className={`glow-text-blue`}>{props.NFT.creator.name}</span>
-            </div>
+            { props.showCreator && 
+                <div className={styles.Creator}>
+                    <div
+                        style={{backgroundImage: `url(${props.NFT.creator.img})`}} 
+                    ></div>
+                    <span className={`glow-text-blue`}>{props.NFT.creator.name}</span>
+                </div>
+            }
             <h5 className={`glow-text-blue`}>{props.NFT.name}</h5>
             <div 
                 className={styles.NFT}
@@ -22,15 +24,17 @@ const NFTCard = (props) => {
                     <span className={`glow-text-blue`}>Last Traded Price</span>
                     <span className={`glow-text-blue`}>{props.NFT.price} WILD</span>
                 </div>
-                <div>
-                    <span className={`glow-text-blue`}>Owned By</span>
-                    <div className={styles.Creator}>
-                        <div
-                           style={{backgroundImage: `url(${props.NFT.owner.img})`}} 
-                        ></div>
-                        <span className={`glow-text-blue`}>{props.NFT.owner.name}</span>
+                { props.showOwner &&
+                    <div>
+                        <span className={`glow-text-blue`}>Owned By</span>
+                        <div className={styles.Creator}>
+                            <div
+                            style={{backgroundImage: `url(${props.NFT.owner.img})`}} 
+                            ></div>
+                            <span className={`glow-text-blue`}>{props.NFT.owner.name}</span>
+                        </div>
                     </div>
-                </div>
+                }
             </div>
         </div>
     )
--- ZNS-react/src/containers/Shop/Shop.js
+++ ZNS-react/src/containers/Shop/Shop.js
@@ -13,8 +13,6 @@ const Shop = (props) => {
     const [ NFTs, setNFTs ] = useState(NFTService.getOwnedBy(loggedInAs))
     const [ selected, setSelected ] = useState('ownedBy')
 
-    console.log(selected)
-
     const getOwnedBy = () => {
         setNFTs(NFTService.getOwnedBy(loggedInAs))
         setSelected('ownedBy')
@@ -23,7 +21,21 @@ const Shop = (props) => {
     const getCreatedBy = () => {
         setNFTs(NFTService.getCreatedBy(loggedInAs))
         setSelected('createdBy')
-        console.log(selected)
+    }
+
+    const getOffers = () => {
+        setNFTs([])
+        setSelected('offers')
+    }
+
+    const selectedCss = {
+        borderBottom: '1px solid #E0BAFF',
+        fontWeight: 400,
+    }
+
+    const defaultCss = {
+        fontWeight: 400,
+        color: 'white'
     }
 
     return(
@@ -32,16 +44,29 @@ const Shop = (props) => {
                 <TextButton 
                     onClick={getOwnedBy}
                     selected={selected === 'ownedBy'}
+                    style={selected === 'ownedBy' ? selectedCss : defaultCss}
                 >NFTs You Own</TextButton>
                 <TextButton
                     onClick={getCreatedBy}
                     selected={selected === 'createdBy'}
+                    style={selected === 'createdBy' ? selectedCss : defaultCss}
                 >NFTs You've Made</TextButton>
+                <TextButton
+                    onClick={getOffers}
+                    selected={selected === 'offers'}
+                    style={selected === 'offers' ? selectedCss : defaultCss}
+                >Offers</TextButton>
                 {/* <TextButton toggleable={true}>Offers</TextButton> */}
             </div>
             <ul>
                 { NFTs.map(n => 
-                    <li key={n.name}><NFTCard NFT={n} /></li>
+                    <li key={n.name}>
+                    <NFTCard 
+                        NFT={n} 
+                        showCreator={selected !== 'createdBy'}
+                        showOwner={selected !== 'ownedBy'}
+                    />
+                    </li>
                 ) }
             </ul>
         </div>
