b5ac61978b2fd9099be9a356330a034453e6e2f4
Brett <colbr243@gmail.com>
2021-04-10 13:41:27 +1200

    💄 Got sidebar looking better, just need the raw SVG icons

--- ZNS-react/src/components/SideBar/SideBar.js
+++ ZNS-react/src/components/SideBar/SideBar.js
@@ -2,32 +2,21 @@ import React from 'react'
 
 import SideBarStyles from './SideBar.module.css'
 
-import icon1 from './assets/1.svg'
-import icon2 from './assets/2.svg'
-import icon3 from './assets/3.svg'
-import icon4 from './assets/4.svg'
+import hand from './assets/hand.svg'
+import scales from './assets/scales.svg'
+import graph from './assets/graph.svg'
+import telescope from './assets/telescope.svg'
 
 const SideBar = (props) => {
 
     return (
-    	<div className={SideBarStyles.sideBar}>
-    		<div className={SideBarStyles.navigator}>
-				<ul className={SideBarStyles.dots}>
-					<li className='purple-border-blur'></li>
-				</ul>
-				<ul className={SideBarStyles.icons + ' purple-border-blur'}>
-					<li>
-						<button><img alt="channels" src={icon1} /></button>
-					</li>
-					<li>
-						<button><img alt="tasks" src={icon2} /></button>
-					</li>
-					<li>
-						<button><img alt="feed" src={icon3} /></button>
-					</li>
-					<li>
-						<button><img alt="members" src={icon4} /></button>
-					</li>
+    	<div className={SideBarStyles.SideBar}>
+    		<div className={`${SideBarStyles.Navigator}  border-primary blur`}>
+				<ul className={SideBarStyles.Icons}>
+					<li style={{backgroundImage: `url(${hand})`}}></li>
+					<li style={{backgroundImage: `url(${scales})`}}></li>
+					<li style={{backgroundImage: `url(${graph})`}}></li>
+					<li style={{backgroundImage: `url(${telescope})`}}></li>
 				</ul>
 			</div>
     	</div>
