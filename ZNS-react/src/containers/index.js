7fae4a56ff117094d2d404f1dbbc861403f060dc
Brett <colbr243@gmail.com>
2021-04-10 14:55:34 +1200

    :sparkles: Added a container index

---
+++ ZNS-react/src/containers/index.js
@@ -0,0 +1,12 @@
+/*
+ * @author Brett Collins
+ *
+ * This file implements the barrel pattern
+ * All containers are exported from here, so that they
+ * can be imported from one consistent spot. Restructuring 
+ * the project is easier, because the ref to each container is
+ * in one place.
+ *
+ */
+
+export { default as Shop } from './Shop/Shop.js'
